8897a3a08da69b1378fe320627720a5647bd6daf
Chetan baliyan <chetanbaliyan10@gmail.com>
2023-07-29 17:15:59 +0530

    minor fix

--- src/audio/getTimestamp.ts
+++ src/audio/getTimestamp.ts
@@ -1,19 +1,19 @@
-import { whisper } from 'whisper-node'
+// import { whisper } from 'whisper-node'
 
-const getTimestampFromAudio = async (audioPath: string, modelName = 'tiny.en') => {
-	const filePath = 'ttest.wav' // required
+// const getTimestampFromAudio = async (audioPath: string, modelName = 'tiny.en') => {
+// 	const filePath = 'ttest.wav' // required
 
-	const options = {
-		modelName: 'tiny.en', // default
-		// modelPath: "./models/ggml-tiny.en.bin", // use model in a custom directory
-		whisperOptions: {
-			gen_file_txt: false, // outputs .txt file
-			gen_file_subtitle: true, // outputs .srt file
-			gen_file_vtt: false, // outputs .vtt file
-			timestamp_size: 10, // amount of dialogue per timestamp pair
-		},
-	}
+// 	const options = {
+// 		modelName: 'tiny.en', // default
+// 		// modelPath: "./models/ggml-tiny.en.bin", // use model in a custom directory
+// 		whisperOptions: {
+// 			gen_file_txt: false, // outputs .txt file
+// 			gen_file_subtitle: true, // outputs .srt file
+// 			gen_file_vtt: false, // outputs .vtt file
+// 			timestamp_size: 10, // amount of dialogue per timestamp pair
+// 		},
+// 	}
 
-	const transcript = await whisper(filePath, options)
-	console.log(transcript)
-}
+// 	const transcript = await whisper(filePath, options)
+// 	console.log(transcript)
+// }
