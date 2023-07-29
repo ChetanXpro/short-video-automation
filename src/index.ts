a8269e594778290668769b7f3df8dff72432ced1
Chetan baliyan <chetanbaliyan10@gmail.com>
2023-07-29 19:01:10 +0530

    feat: get transcript with whisper.cpp

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 import { createShortScript } from './audio/videoScript'
 import { createAudio } from './audio/elevenAudio'
+import { convertToWav, whisper } from './transcript/transcribe'
+import path from 'path'
+import shell from 'shelljs'
 
-createShortScript({ language: 'english', topic: 'historical fact' })
+// createShortScript({ language: 'english', topic: 'historical fact' })
 
 const generateYoutubeShort = async () => {
 	try {
@@ -20,4 +23,11 @@ const generateYoutubeShort = async () => {
 	}
 }
 
-generateYoutubeShort()
+// generateYoutubeShort()
+const inputFilePath = path.join(__dirname, '..', 'basicaudio.mp3')
+// console.log(AUDIOPATH)
+const outputFilePath = path.join(__dirname, '..', 'basicaudio.wav')
+// convertToWav(inputFilePath, outputFilePath).then(e => {
+// 	console.log(e)
+// })
+whisper(outputFilePath)
