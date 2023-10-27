d322ba622db8342293457de06ebe17ba336225c4
chetan <chetan@chetans-MacBook-Air.local>
2023-10-27 23:18:35 +0530

    feat: add flow

--- src/index.ts
+++ src/index.ts
@@ -25,29 +25,29 @@ const outputVideoFilePath = path.join(__dirname, '..', 'shorts', 'test.mp4')
 
 const generateYoutubeShort = async (language: string, topic: string) => {
 	try {
-		// const script = await createShortScript({ language: language, topic: topic })
+		const script = await createShortScript({ language: language, topic: topic })
 
-		// console.log('SCRIPT GENERATED: ', script)
+		console.log('SCRIPT GENERATED: ', script)
 
-		// if (!script) throw new Error('Script not generated')
+		if (!script) throw new Error('Script not generated')
 
-		// await createAudio({ script, language, outputFilePath: inputFilePath })
+		await createAudio({ script, language, outputFilePath: inputFilePath })
 
-		// console.log('AUDIO GENERATED SUCCESSFULLY', 'basicaudio.mp3')
+		console.log('AUDIO GENERATED SUCCESSFULLY', 'basicaudio.mp3')
 
-		// await convertToWav(inputFilePath, outputFilePath)
+		await convertToWav(inputFilePath, outputFilePath)
 
-		// await whisper(outputFilePath)
+		await whisper(outputFilePath)
 
 		// return
 
-		// console.log('MERGING AUDIO AND VIDEO')
+		console.log('MERGING AUDIO AND VIDEO')
 
-		// await mergeAudio({
-		// 	videoFilePath,
-		// 	audioFilePath: outputFilePath,
-		// 	outputVideoPath: outputVideoFilePath,
-		// })
+		await mergeAudio({
+			videoFilePath,
+			audioFilePath: outputFilePath,
+			outputVideoPath: outputVideoFilePath,
+		})
 
 		// return
 
@@ -65,62 +65,9 @@ const generateYoutubeShort = async (language: string, topic: string) => {
 		})
 
 		return
-		// uploadVideos('facts', 'facts', ['#facts', '#trending', '#shorts'], outputVideoFilePath)
+		// Upload to youtube
 		uploadFile('videos', Math.random() + 'new.mp4', outputVideoFilePath).catch(console.error)
 	} catch (error) {
 		console.log('Error in createShortScript: ', error)
 	}
 }
-
-// const containerName = 'videos'
-// const blobName = 'new.mp4'
-// const filePath = '/home/chetan/code/ts-content-gpt/new.mp4'
-
-// uploadFile(containerName, blobName, filePath).catch(console.error)
-
-// app.use(express.json())
-
-generateYoutubeShort('english', 'ukrain fact')
-
-const queries = [
-	{ Query: 'Great Wall of China', timestamp: '1 -> 3' },
-	{ Query: 'Chinese Empire', timestamp: '11 -> 12' },
-	// { Query: 'Ming Dynasty', timestamp: '7 -> 8' },
-	// { Query: 'Chinese Empire', timestamp: '11 -> 12' },
-	// { Query: 'brick, tamperth, and stone', timestamp: '17 -> 18' },
-]
-
-// mergeAudio({
-// 	videoFilePath,
-// 	audioFilePath: outputFilePath,
-// 	outputVideoPath: outputVideoFilePath,
-// })
-
-// imageProccessing({ language: '', queries })
-
-// downloadImages(queries)
-
-// app.post('/generate', async (req, res) => {
-// 	try {
-// 		const { language, topic } = req.body
-// 		if (!language || !topic) return res.status(500).send('Provide language and topic')
-// 		const startTime: number = new Date().getTime()
-
-// 		await generateYoutubeShort('english', 'indian fact')
-
-// 		const endTime: number = new Date().getTime()
-// 		const executionTimeMilliseconds = endTime - startTime
-
-// 		const executionTimeSeconds = executionTimeMilliseconds / 1000
-
-// 		console.log(`Function execution time: ${executionTimeSeconds.toFixed(2)} seconds`)
-// 		res.status(200).json({ status: `Function execution time: ${executionTimeSeconds.toFixed(2)} seconds` })
-// 	} catch (error) {
-// 		console.log('Error in /generate: ', error)
-// 		res.status(500).send('Error in /generate')
-// 	}
-// })
-
-// app.listen(3000, () => {
-// 	console.log('Server running on port 3000')
-// })
